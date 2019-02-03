12bcb7705a4ca3d24b1880e428003c5f51829f3e
Olivia Osborn <olioz95@gmail.com>
2019-02-02 19:33:16 -0500

    created login page

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# olivia-osborn-front-end-arch
+Pitch: Service workers need a way to collect tips directly, rather than through their employers. This app will allow payment processing directly to service workers as a tip.
+
+MVP: Service Employee can login and create, read and update a profile. Regular User can login and select a service employee and give them a tip.
+
+Stretch: Implement a payment feature that actually allows money exchange with a service like, paypal or stripe or venmo.
---
+++ tipsease/src/components/Login.js
@@ -0,0 +1,75 @@
+import React from "react";
+import styled from "styled-components";
+
+const LoginCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 400px;
+    margin: 10% auto;
+    padding-bottom: 40px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+`
+const LoginLogo = styled.img`
+    height: 200px;
+    width: auto;
+`
+
+const StyledInput = styled.input`
+    font-size: 1rem;
+    padding: 5px;
+    margin-bottom: 5px;
+`
+
+const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const Submit = styled.button`
+    background: lightgray;
+    font-size: 1rem;
+    border: 1px solid gray;
+    width: 50%;
+    &:hover {
+        cursor: pointer;
+        background: gray;
+    }
+`
+
+const SignUp = styled.button`
+background: white;
+font-size: 1rem;
+border: white;
+margin-top: 20px;
+&:hover {
+    cursor: pointer;
+    color: gray;
+}
+`
+
+function Login(props) {
+    return (
+        <LoginCard>
+            <LoginLogo src={require('../img/tipease4.png')} alt=""/>
+            <StyledForm>
+                <StyledInput
+                    type="text"
+                    placeholder="email"
+                    name="email"
+                />
+                <StyledInput
+                    type="password"
+                    placeholder="password"
+                    name="password"
+                />       
+                <Submit>Submit</Submit>         
+            </StyledForm>
+            <SignUp>Sign up</SignUp>
+        </LoginCard>
+    )
+}
+
+export default Login;
